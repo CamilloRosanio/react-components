f0480f2e282fcb573d088f6d04ecb2102eda1bb3
MyUsername <rosanio.c@gmail.com>
2024-11-29 10:36:33 +0100

    CORREZIONE - Spiegazione e commenti module.css/.css (componente HEADER come esempio)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,12 @@
+// Per applicare lo stile dal proprio foglio CSS devo importare il foglio di stile.
+// Importando, saranno applicate automatiamente le caratteristiche in base ai selettori scritti sul foglio.
+// REACT fa differenza tra "nome.css" e "nome.module.css"
+// Le regole scritte nel MODULE vengono applicate solo e unicamente al COMPONENT che lo importa.
+
+// Se lo importo come variabile (styles) applica le caratteristiche ai TAG, ma non alle CLASS a meno che non inserisca le CLASS come variabile {}
+
 import styles from './Header.module.css';
+
 
 function Header() {
 
